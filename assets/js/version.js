0e34564d9cdf0992d68e9f4f19960076e74ffd60
Jonatas Costa <jonatas3515@gmail.com>
2026-08-31 19:12:56 -0300

    fix(store): exibir opções de mini-salgados no modal de combos

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
-window.APP_VERSION = 1788213836552;
+window.APP_VERSION = 1788214357807;
 console.log('[VERSION] App version:', window.APP_VERSION);
