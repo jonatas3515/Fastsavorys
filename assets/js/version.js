992e05b6eea45e38b42e887e8950b23c6320d337
Jonatas Costa <jonatas3515@gmail.com>
2026-08-31 19:30:17 -0300

    fix(store): corrigir deteccao do Combo Explosao para carregar salgados grandes

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
-window.APP_VERSION = 1788214768888;
+window.APP_VERSION = 1788215193318;
 console.log('[VERSION] App version:', window.APP_VERSION);
