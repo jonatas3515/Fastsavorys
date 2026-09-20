7c0265e78210cd1c55eb0b6df9e8c50c18204197
Jonatas Costa <jonatas3515@gmail.com>
2026-09-20 18:59:53 -0300

    feat(admin): tornar o Kanban fluido para telas grandes e responsivo em notebooks

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Auto-generated version file - DO NOT EDIT MANUALLY
-window.APP_VERSION = 1789939955766;
+window.APP_VERSION = 1789941558900;
 console.log('[VERSION] App version:', window.APP_VERSION);
